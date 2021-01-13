01eadcfc38de9ca3148b29dc48d9900fc8fe064b
HassenMahdi <hassenmahdi77@gmail.com>
2021-01-13 17:07:03 +0100

    docker config

--- backend/config.py
+++ backend/config.py
@@ -5,24 +5,15 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    MONGO_URI = "mongodb://dcm-consmos" \
-                ":pUQRAZMYnTiYikWTxjcq7zQch27litMHCSJnHOu9XCssYxBqVRWmMpd8sSnd0G7w66dQ7GMS4UK8iAvOsoBGtw==@dcm" \
-                "-consmos.mongo.cosmos.azure.com:10255/dcm?ssl=true&replicaSet=globaldb&retrywrites=false" \
-                "&maxIdleTimeMS=120000&appName=@dcm-consmos@"
-    AIRFLOW_ENDPOINT="http://localhost:5005/admin/"
 
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = ""
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://dcm-consmos:pUQRAZMYnTiYikWTxjcq7zQch27litMHCSJnHOu9XCssYxBqVRWmMpd8sSnd0G7w66dQ7GMS4UK8iAvOsoBGtw==@dcm-consmos.mongo.cosmos.azure.com:10255/dcm?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@dcm-consmos@")
     AIRFLOW_LOG_FOLDER = os.getenv("AIRFLOW_LOG_FOLDER","C://Users//Hassen//Desktop//DCM//dcm-airflow//airflow-logs")
     SQLALCHEMY_DATABASE_URI = os.getenv('AIRFLOW__CORE__SQL_ALCHEMY_CONN', 'postgresql+psycopg2://airflow:airflow@localhost:5433/airflow')
-    AIRFLOW_ENDPOINT = os.getenv('AIRFLOW_ENDPOINT', 'http://webserver:8080/admin/')
+    AIRFLOW_ENDPOINT = os.getenv('AIRFLOW_ENDPOINT', 'http://localhost:5005/admin/')
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('AIRFLOW__CORE__SQL_ALCHEMY_CONN', 'postgresql+psycopg2://airflow:airflow@localhost:5433/airflow')
-
 
 config = dict(
     dev=DevelopmentConfig,
