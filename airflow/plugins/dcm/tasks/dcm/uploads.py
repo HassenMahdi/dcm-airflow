6bbec8503a0dc74e54df95a087267eac028d82a4
wajihkatrou <wajih.katrou@berexia.com>
2021-02-09 16:04:58 +0100

    Adding collection upload handdler

--- airflow/plugins/dcm/tasks/dcm/uploads.py
+++ airflow/plugins/dcm/tasks/dcm/uploads.py
@@ -1,7 +1,8 @@
-from dcm.tasks.dcm import DcmService
-import requests
 import os
-import json
+
+import requests
+from airflow.plugins.dcm.tasks.dcm import DcmService
+
 
 class BaseUploadHandler(DcmService):
     base_url = os.getenv("DCM_SERVICE__UPLOAD")
@@ -14,8 +15,6 @@ class BaseUploadHandler(DcmService):
         else:
             return super().start()
             
-
-
 
 class UploadConnectorHandler(BaseUploadHandler):
     upload_connector_types = ["BLOB_STORAGE_UPLOAD_CONNECTOR", "SQL_UPLOAD_CONNECTOR", "POSTGRES_UPLOAD_CONNECTOR"]
@@ -33,3 +32,31 @@ class UploadConnectorHandler(BaseUploadHandler):
             return {'status':'success'}
         else:
             raise Exception('Upload has Failed')
+
+
+class UploadCollectionConnectorHandler(BaseUploadHandler):
+
+    upload_collection_connector_type = "COLLECTION_UPLOAD"
+    cleansing_url = os.getenv("DCM_SERVICE_CLEANSING")
+
+    def run(self, params):
+        # FORM PAYLOAD
+        file_id = self.input['file_id']
+        sheet_id = self.input['sheet_id']
+        mapping_id = self.input["mapping_id"]
+        payload = {
+            **params,
+            "file_id": file_id,
+            "sheet_id": sheet_id,
+            "mapping_id": mapping_id
+        }
+        run_cleansing = requests.post(url=f"{self.cleansing_url}", json=payload)
+        job_status = requests.get(url=f"{self.cleansing_url}/metadata/{run_cleansing.json()['job_id']}")
+        if job_status.json()["totalErrors"] == 0:
+            run_upload = requests.post(url=f"{self.base_url}connector", json=payload)
+            if run_upload.status_code == 200:
+                return {'status':'success'}
+            else:
+                raise Exception('Upload has Failed')
+        else:
+            return {"status": "cleansing error"}
