869d77787ed92c9828ab2c843e0baf745e2c2981
Wajdovic <wajdibenabderrahim@gmail.com>
2021-02-21 21:17:30 +0100

    Update uploads.py

--- airflow/plugins/dcm/tasks/services/uploads.py
+++ airflow/plugins/dcm/tasks/services/uploads.py
@@ -57,6 +57,7 @@ class UploadCollectionConnectorHandler(BaseUploadHandler):
         }
 
         run_cleansing = requests.post(url=f"{self.cleansing_url}", json=cleansing_payload)
+        print("---------------->"+str(run_cleansing))
         job_id = run_cleansing.json()['job_id']
         job_status = requests.get(url=f"{self.cleansing_url}/metadata/{job_id}")
         if job_status.json()["totalErrors"] == 0:
