19c6f12d322cd6528a32f6f1d2927ffc9e0de313
KarimBourass <bourasskarim9@gmail.com>
2021-08-26 13:55:32 +0100

    Change to local mongo database

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
-    MONGO_URI = "mongodb://root:Bxia2020DaaTa1920CAvlmd@20.74.14.235:27017/dcm?authSource=admin&readPreference=primary&ssl=false"
+    #MONGO_URI = "mongodb://root:Bxia2020DaaTa1920CAvlmd@20.74.14.235:27017/dcm?authSource=admin&readPreference=primary&ssl=false"
+    MONGO_URI = "mongodb://host.docker.internal:27017/dcm?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
     # AIRFLOW_LOG_FOLDER = os.getenv("AIRFLOW_LOG_FOLDER","C://Users//Hassen//Desktop//DCM//dcm-airflow//airflow-logs")
     AIRFLOW_LOG_FOLDER = os.getenv("AIRFLOW_LOG_FOLDER","C://DCM//dcm-airflow//airflow-logs")
     SQLALCHEMY_DATABASE_URI = os.getenv('AIRFLOW__CORE__SQL_ALCHEMY_CONN', 'postgresql+psycopg2://airflow:airflow@localhost:5433/airflow')
